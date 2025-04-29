60200b0b0681e819093140341d3ae8a4490f3750
nguyenhuyit2308 <huyreunglang123@gmail.com>
2025-04-29 10:01:03 +0700

    Update lần 2 listService

--- app/Http/Controllers/CRUD_ServiceController.php
+++ app/Http/Controllers/CRUD_ServiceController.php
@@ -10,7 +10,7 @@ class CRUD_ServiceController extends Controller
     // Danh sách dịch vụ
     public function listService()
     {
-        $service = Service::all();
+        $service = Service::paginate(6);
         if ($service->isEmpty()) {
             return view('crud_service.list', compact('service'))->with('error', 'Không có dịch vụ nào!!!');
         }
