bb3179322192f427990026be0c14ad3a62fd7d92
nguyenhuyit2308 <huyreunglang123@gmail.com>
2025-05-12 14:52:55 +0700

    delete count at login page

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -33,7 +33,6 @@ class LoginController extends Controller
             Auth::login($account); // Đăng nhập
 
             // Lấy số lượng booking
-            $bookingCount = BookingService::where('customer_id', $account->id)->count();
             $bookingCount = BookingService::where('customer_id', auth()->user()->id)
                 ->where('status', 'pending')
                 ->count();
